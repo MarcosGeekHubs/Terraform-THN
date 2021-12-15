c0d82e7e0a55f141495522d9e2be78515a3adb0c
Marcos Palacios <m.palacios@geekshubs.com>
2021-12-15 12:58:07 +0100

    For ejemplo

---
+++ 0800-Expresiones-For/01Ejemplo/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+provider "aws" {
+  profile = "Marcos_Palacios"
+  region  = "eu-west-1"
+}
+
+variable "users" {
+  type = set(string)
+  //default = [ "Marcos","Juan" ]
+}
+
+resource "aws_iam_user" "example_user" {
+ // count = length(var.users)
+  for_each = var.users
+  name = each.value
+}
+
+output "arn_all_users" {
+  value = [for user in aws_iam_user.example_user : user.arn]
+}
