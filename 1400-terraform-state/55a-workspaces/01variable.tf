dee674e987b42538a765d3a8ba94990242c9b07c
Marcos Palacios <m.palacios@geekshubs.com>
2021-12-21 12:17:35 +0100

    workspace

---
+++ 1400-terraform-state/55a-workspaces/01variable.tf
@@ -0,0 +1,15 @@
+variable "server_type" {
+  type = string
+}
+
+ variable "server_profile" {
+   type = string
+ }
+
+ variable "server_port" {
+   type = number
+ }
+
+ variable "region_server" {
+   type = string
+ }
